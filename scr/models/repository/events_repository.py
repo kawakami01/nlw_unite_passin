8cebd491f204209aac7f1327d34c20c9bdb4103e
Kawakami <migskawakami@gmail.com>
2024-04-02 20:27:15 -0300

    improve: adding try in events repository

--- scr/models/repository/events_repository.py
+++ scr/models/repository/events_repository.py
@@ -1,29 +1,41 @@
 from typing import Dict
 from scr.models.settings.connection import db_connection_handler
 from scr.models.entities.events import Events
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class EventsRepository:
     def insert_event(self, events_info: Dict) -> Dict:
         with db_connection_handler as database:
-            event = Events(
-                id=events_info.get("uuid"),
-                title=events_info.get("title"),
-                details=events_info.get("details"),
-                slug=events_info.get("slug"),
-                maximum_attendees=events_info.get("maximum_attendees.get"),
-            )
-            database.session.add(event)
-            database.session.commit()
+            try:
+                event = Events(
+                    id=events_info.get("uuid"),
+                    title=events_info.get("title"),
+                    details=events_info.get("details"),
+                    slug=events_info.get("slug"),
+                    maximum_attendees=events_info.get("maximum_attendees.get"),
+                )
+                database.session.add(event)
+                database.session.commit()
 
-            return events_info
+                return events_info
+            except IntegrityError:
+                raise Exception('Event already registered!')
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
 
     def get_event_by_id(self, event_id: str) -> Events:
         with db_connection_handler as database:
-            event = (
-                database.session
-                .query(Events)
-                .filter(Events.id == event_id)
-                .one()
-            )
-            return event
+            try:
+                event = (
+                    database.session
+                    .query(Events)
+                    .filter(Events.id == event_id)
+                    .one()
+                )
+                return event
+            except NoResultFound:
+                return None
