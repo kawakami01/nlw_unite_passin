a38384068d0670c6e18851b26b3926e6add4cabb
Kawakami <migskawakami@gmail.com>
2024-04-03 11:56:09 -0300

    feat: implementing get event route

--- scr/main/server/server.py
+++ scr/main/server/server.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from scr.models.settings.connection import db_connection_handler
+from scr.main.routes.event_routes import event_route_bp
 
 db_connection_handler.connect_to_database()
 
 app = Flask(__name__)
 CORS(app)
 
-from scr.main.routes.event_routes import event_route_bp
+
 app.register_blueprint(event_route_bp)
