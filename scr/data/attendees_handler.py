5e5ef63fb0474e338b9c7608ec9b1952274bfad6
Kawakami <migskawakami@gmail.com>
2024-04-04 10:02:33 -0300

    implementing attendees handler with attendee badge route

---
+++ scr/data/attendees_handler.py
@@ -0,0 +1,44 @@
+import uuid
+from scr.models.repository.attendees_repository import AttendeesRepository
+from scr.models.repository.events_repository import EventsRepository
+from scr.http_types.http_request import HttpRequest
+from scr.http_types.http_response import HttpResponse
+
+
+class AttendeesHandler():
+    def __init__(self) -> None:
+        self.__attendees_repository = AttendeesRepository()
+        self.__events_repository = EventsRepository()
+        
+    def registry(self, https_request: HttpRequest) -> HttpResponse:
+        body = https_request.body
+        event_id = https_request.param["event_id"]
+        
+        event_attendees_count = self.__events_repository.count_event_attendees(event_id)
+        if (
+            event_attendees_count["attendees_amount"]
+            and event_attendees_count["maximum_attendees"] < event_attendees_count["attendees_amount"]
+        ): raise Exception("The event is crowded!")
+        
+        body["uuid"] = str(uuid.uuid4())
+        body["event_id"] = event_id
+        self.__attendees_repository.insert_attendee(body)
+        
+        return HttpResponse(body=None, status_code=201)
+        
+    def find_attendee_badge(self, https_request: HttpRequest) -> HttpResponse:
+        attendee_id = https_request.param["attendee_id"] # mudar isso se der erro
+        badge = self.__attendees_repository.get_attendee_badge_by_id(attendee_id)
+        if not badge: raise Exception("Attendee not found!")
+        
+        return HttpResponse(
+            body={
+                "badge": {
+                    "name": badge.name,
+                    "email": badge.name,
+                    "event_title": badge.title
+                }
+            },
+            status_code=200
+        )
+    
--- scr/main/server/server.py
+++ scr/main/server/server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from scr.models.settings.connection import db_connection_handler
 from scr.main.routes.event_routes import event_route_bp
+from scr.main.routes.attendees_routes import attendees_route_bp
+
 
 db_connection_handler.connect_to_database()
 
@@ -10,3 +12,4 @@ CORS(app)
 
 
 app.register_blueprint(event_route_bp)
+app.register_blueprint(attendees_route_bp)
